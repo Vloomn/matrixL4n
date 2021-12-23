025e4377c498f95472d92e61843c9827c4395334
OmerBM1998 <36604212+OmerBM1998@users.noreply.github.com>
2021-12-23 11:47:14 +0200

    added

---
+++ Log4NetTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+
+namespace AdvanceLib
+{
+    public class Log4NetTest
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly Stopwatch stopwatch = new Stopwatch();
+        public static void StopwatchLog(int num)
+        {
+            //BasicConfigurator.Configure();
+            if (num < 0)
+            {
+                return;
+            }
+            // Create new stopwatch.
+
+            // Begin timing.
+            stopwatch.Start();
+
+            // Do something.
+            int i = 0;
+            while (i < num)
+            {
+                i++;
+            }
+
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+            string output = DateTime.Now.ToString("M/d/yyyy") + " - Input:" + num.ToString() + " - Time: " + stopwatch.Elapsed;
+            log.Info(output);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using AdvanceLib;
+using System;
+
+namespace ShowCase
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type a number...");
+
+            while (true)
+            {
+                int num;
+                if (int.TryParse(Console.ReadLine(), out num))
+                {
+                    Log4NetTest.StopwatchLog(num);
+                }
+                else
+                {
+                    Console.WriteLine("couldn't recognize a number, please try again.");
+                }
+            }
+        }
+    }
+}
